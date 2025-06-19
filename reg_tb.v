8a752b638529ae54363ea37a6a93f6825dfb6c1b
StefanosZKS <76497376+StefanosZKS@users.noreply.github.com>
2025-06-19 21:13:48 +0300

    Update reg_tb.v

--- reg_tb.v
+++ reg_tb.v
@@ -1,4 +1,9 @@
+`timescale 10ns/10ns
+
 module tb;
+  reg clk;
+  reg rst;
+  
   reg [4:0] raA, raB, wa;
   reg [127:0] wd;
   reg wen;
@@ -11,3 +16,14 @@ module tb;
 
   wire [127:0] rdA, rdB;
   wire [8:0] vl;
+
+  vRegFile vrgfl (clk, rst, raA, raB, wa, wd, wen, vl_in, vl_wem, vtype_in, vtype_wen, rdA, rdB, vl, vtype);
+
+  always
+    #1  clk = ~clk;
+
+    initial begin
+      $dumpfile("tb.vcd");
+      $dumpvars(0, tb);
+
+      ra
