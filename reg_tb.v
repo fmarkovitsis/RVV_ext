4e78c0a30c7d787e7381c0913a3d256739ae3ee5
StefanosZKS <76497376+StefanosZKS@users.noreply.github.com>
2025-06-19 20:56:38 +0300

    Update reg_tb.v

--- reg_tb.v
+++ reg_tb.v
@@ -1,2 +1,13 @@
 module tb;
-  reg 
+  reg [4:0] raA, raB, wa;
+  reg [127:0] wd;
+  reg wen;
+
+  reg [8:0] vl_in;
+  reg vl_wen;
+
+  reg [6:0] vtype_in;
+  reg vtype_wen;
+
+  wire [127:0] rdA, rdB;
+  wire [8:0] vl;
