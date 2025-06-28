4c980ed04aae3c5483d5ec74996cf25bec16cb27
Charis Zachariadis <124374442+czachariadis@users.noreply.github.com>
2025-06-28 12:59:12 +0300

    changed name

--- valu.v
+++ valu.v
@@ -7,7 +7,7 @@ module vALU (
         reg_scalar_in,
         valu_op,
         SEW,
-        reg_dest
+        result
    );
 
     input[63:0]    reg_in1, reg_in2;
@@ -15,7 +15,7 @@ module vALU (
     input[3:0]      valu_op;
     input[2:0]      SEW;
 
-    output reg [63:0] reg_dest;         
+    output reg [63:0] result;         
 
     parameter [6:0] VLEN = 7'd64; 
     integer i;
@@ -25,31 +25,31 @@ module vALU (
 
     always @(*) begin
 
-        reg_dest = 64'b0;
+        result = 64'b0;
 
         case (valu_op)
             4'b0000: begin   //000 -> elementwise addition
                 case (SEW)
                     3'b000:   begin
                         for (i=0; i < (VLEN>>3); i=i+1) begin
-                            reg_dest[8*i +: 8] = reg_in1[8*i +: 8] + reg_in2[8*i +: 8];
+                            result[8*i +: 8] = reg_in1[8*i +: 8] + reg_in2[8*i +: 8];
                         end
                     end
                     3'b001: begin
                         for (i=0; i < (VLEN>>4); i=i+1) begin
-                            reg_dest[16*i +: 16] = reg_in1[16*i +: 16] + reg_in2[16*i +: 16];
+                            result[16*i +: 16] = reg_in1[16*i +: 16] + reg_in2[16*i +: 16];
                         end
                     end     
                     3'b010: begin
                         for (i=0; i < (VLEN>>5); i=i+1) begin
-                            reg_dest[32*i +: 32] = reg_in1[32*i +: 32] + reg_in2[32*i +: 32];
+                            result[32*i +: 32] = reg_in1[32*i +: 32] + reg_in2[32*i +: 32];
                         end
                     end
                     3'b011: begin
-                        reg_dest = reg_in1 + reg_in2;
+                        result = reg_in1 + reg_in2;
                     end
                     default: begin
-                        reg_dest = 64'b0; //to avoid latches
+                        result = 64'b0; //to avoid latches
                     end
                 endcase   
             end
@@ -58,24 +58,24 @@ module vALU (
                 case (SEW)
                     3'b000: begin
                         for (i=0; i < (VLEN>>3); i=i+1) begin
-                            reg_dest[8*i +: 8] = reg_in1[8*i +: 8] + reg_scalar_in[7:0];
+                            result[8*i +: 8] = reg_in1[8*i +: 8] + reg_scalar_in[7:0];
                         end
                     end
                     3'b001: begin
                         for (i=0; i < (VLEN>>4); i=i+1) begin
-                            reg_dest[16*i +: 16] = reg_in1[16*i +: 16] + reg_scalar_in[15:0];
+                            result[16*i +: 16] = reg_in1[16*i +: 16] + reg_scalar_in[15:0];
                         end
                     end
                     3'b010: begin
                         for (i=0; i < (VLEN>>5); i=i+1) begin
-                            reg_dest[32*i +: 32] = reg_in1[32*i +: 32] + reg_scalar_in[31:0];
+                            result[32*i +: 32] = reg_in1[32*i +: 32] + reg_scalar_in[31:0];
                         end
                     end
                     3'b011: begin
-                         reg_dest = reg_in1 + reg_scalar_in;
+                         result = reg_in1 + reg_scalar_in;
                     end
                     default: begin
-                        reg_dest = 64'b0; //to avoid latches
+                        result = 64'b0; //to avoid latches
                     end
                 endcase
             end
@@ -84,24 +84,24 @@ module vALU (
                 case (SEW)
                     3'b000:   begin
                         for (i=0; i < (VLEN>>3); i=i+1) begin
-                            reg_dest[8*i +: 8] = reg_in1[8*i +: 8] - reg_in2[8*i +: 8];
+                            result[8*i +: 8] = reg_in1[8*i +: 8] - reg_in2[8*i +: 8];
                         end
                     end
                     3'b001: begin
                         for (i=0; i < (VLEN>>4); i=i+1) begin
-                            reg_dest[16*i +: 16] = reg_in1[16*i +: 16] - reg_in2[16*i +: 16];
+                            result[16*i +: 16] = reg_in1[16*i +: 16] - reg_in2[16*i +: 16];
                         end
                     end     
                     3'b010: begin
                         for (i=0; i < (VLEN>>5); i=i+1) begin
-                            reg_dest[32*i +: 32] = reg_in1[32*i +: 32] - reg_in2[32*i +: 32];
+                            result[32*i +: 32] = reg_in1[32*i +: 32] - reg_in2[32*i +: 32];
                         end
                     end
                     3'b011: begin
-                        reg_dest = reg_in1 - reg_in2;
+                        result = reg_in1 - reg_in2;
                     end
                     default: begin
-                        reg_dest = 64'b0; //to avoid latches
+                        result = 64'b0; //to avoid latches
                     end
                 endcase   
             end
@@ -110,24 +110,24 @@ module vALU (
                 case (SEW)
                     3'b000:   begin
                         for (i=0; i < (VLEN>>3); i=i+1) begin
-                            reg_dest[8*i +: 8] = reg_in1[8*i +: 8] - reg_scalar_in[7:0];
+                            result[8*i +: 8] = reg_in1[8*i +: 8] - reg_scalar_in[7:0];
                         end
                     end
                     3'b001: begin
                         for (i=0; i < (VLEN>>4); i=i+1) begin
-                            reg_dest[16*i +: 16] = reg_in1[16*i +: 16] - reg_scalar_in[15:0];
+                            result[16*i +: 16] = reg_in1[16*i +: 16] - reg_scalar_in[15:0];
                         end
                     end     
                     3'b010: begin
                         for (i=0; i < (VLEN>>5); i=i+1) begin
-                            reg_dest[32*i +: 32] = reg_in1[32*i +: 32] - reg_scalar_in[31:0];
+                            result[32*i +: 32] = reg_in1[32*i +: 32] - reg_scalar_in[31:0];
                         end
                     end
                     3'b011: begin
-                        reg_dest = reg_in1 - reg_scalar_in;
+                        result = reg_in1 - reg_scalar_in;
                     end
                     default: begin
-                        reg_dest = 64'b0; //to avoid latches
+                        result = 64'b0; //to avoid latches
                     end
                 endcase 
             end
@@ -137,27 +137,27 @@ module vALU (
                     3'b000:   begin
                         for (i=0; i < (VLEN>>3); i=i+1) begin
                             temp_mult = $signed(reg_in1[8*i +: 8]) * $signed(reg_in2[8*i +: 8]);
-                            reg_dest[8*i +: 8] = temp_mult[7:0];
+                            result[8*i +: 8] = temp_mult[7:0];
                         end
                     end
                     3'b001: begin
                         for (i=0; i < (VLEN>>4); i=i+1) begin
                             temp_mult = $signed(reg_in1[16*i +: 16]) * $signed(reg_in2[16*i +: 16]);
-                            reg_dest[16*i +: 16] = temp_mult[15:0];
+                            result[16*i +: 16] = temp_mult[15:0];
                         end
                     end     
                     3'b010: begin
                         for (i=0; i < (VLEN>>5); i=i+1) begin
                             temp_mult = $signed(reg_in1[32*i +: 32]) * $signed(reg_in2[32*i +: 32]);
-                            reg_dest[32*i +: 32] = temp_mult[31:0];
+                            result[32*i +: 32] = temp_mult[31:0];
                         end
                     end
                     3'b011: begin
                         temp_mult = $signed(reg_in1) * $signed(reg_in2);
-                        reg_dest = temp_mult[63:0];
+                        result = temp_mult[63:0];
                     end
                     default: begin
-                        reg_dest = 64'b0; //to avoid latches
+                        result = 64'b0; //to avoid latches
                     end
                 endcase 
             end
@@ -167,45 +167,45 @@ module vALU (
                     3'b000:   begin
                         for (i=0; i < (VLEN>>3); i=i+1) begin
                             temp_mult = $signed(reg_in1[8*i +: 8]) * $signed(reg_scalar_in[7:0]);
-                            reg_dest[8*i +: 8] = temp_mult[7:0];
+                            result[8*i +: 8] = temp_mult[7:0];
                         end
                     end
                     3'b001: begin
                         for (i=0; i < (VLEN>>4); i=i+1) begin
                             temp_mult = $signed(reg_in1[16*i +: 16]) * $signed(reg_scalar_in[15:0]);
-                            reg_dest[16*i +: 16] = temp_mult[15:0];
+                            result[16*i +: 16] = temp_mult[15:0];
                         end
                     end     
                     3'b010: begin
                         for (i=0; i < (VLEN>>5); i=i+1) begin
                             temp_mult = $signed(reg_in1[32*i +: 32]) * $signed(reg_scalar_in[31:0]);
-                            reg_dest[32*i +: 32] = temp_mult[31:0];
+                            result[32*i +: 32] = temp_mult[31:0];
                         end
                     end
                     3'b011: begin
                         temp_mult = $signed(reg_in1) * $signed(reg_scalar_in);
-                        reg_dest = temp_mult[63:0];
+                        result = temp_mult[63:0];
                     end
                     default: begin
-                        reg_dest = 64'b0; //to avoid latches
+                        result = 64'b0; //to avoid latches
                     end
                 endcase
             end
 
             4'b0110: begin //and
-                reg_dest = reg_in1 & reg_in2;  
+                result = reg_in1 & reg_in2;  
             end
 
             4'b0111: begin //or
-                reg_dest = reg_in1 | reg_in2;
+                result = reg_in1 | reg_in2;
             end
 
             4'b1000: begin //xor
-                reg_dest = reg_in1 ^ reg_in2;
+                result = reg_in1 ^ reg_in2;
             end
 
             default:    begin
-                            reg_dest = 64'b0;
+                            result = 64'b0;
                         end
         endcase
     end
